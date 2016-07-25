bf11b79cc5f089553f6e3c46462c84b9bf189244
offffh <wuzhiyang87@gmail.com>
2016-07-24 20:00:38 -0700

    a simple demonstration of _exit and exit

--- pthread/exitOr_exitInFork.c
+++ pthread/exitOr_exitInFork.c
@@ -1,3 +1,9 @@
+/*
+ * An example demonstrates the difference between exit and _exit in parent-children processes.
+ * (Implemention is)Different from their introduction, the exit and _exit both don't close the files the processed shared immediately.Instead, the kernel will close the files which will not be used by any process any more.
+ * ^-^: A file are shared between processes and they just have different file pointers(FILE *) which point to the same file. We can consider that the file pointers are similar with the smart pointer shared_ptr of C++ language. That means every _exit or exit just destroy its file pointers and decrease the file's ref-count. When the ref-count is zero aka there is no file pointer pointing to the file, the file will be closed at now.
+ */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -34,7 +40,9 @@ int main()
 	}
 
 	/* parent process */
-	fprintf(out, "c");
+	fprintf(out, "c");/* the file pointer 'out' could always be manipulated after _exit or exit above.
+						 Because, it's unnessary to close the file immediately when _exit and exit, 
+						 the linux kernel would close them when the process's termination. */
 	printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
 	exit(0);
 
